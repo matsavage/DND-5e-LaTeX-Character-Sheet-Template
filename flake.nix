bc41581d119a6251fd2db6c903183055447c56eb
Soispha <soispha@vhack.eu>
2023-04-30 15:28:50 +0200

    Feat(flake): Add nix flake
    A nix flake is a reproducible way to describe the building process
    to [nix](https://nixos.org/).
    See https://nixos.org/download.html, on how to install nix. Afterwards
    running `nix build` in the repository will result in a successful build.
    
    Flakes and Nix-Commands are still experimental so they must be enabled
    as described at https://nixos.wiki/wiki/Flakes#Enable_flakes.

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "Latex DnD 5e character sheet";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+nixpkgs_old.url = "github:NixOS/nixpkgs/release-22.11";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs = {};
+    };
+
+    dnd = {
+      url = "github:rpgtex/DND-5e-LaTeX-Template";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    nixpkgs_old,
+    flake-utils,
+    dnd,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs_old = nixpkgs_old.legacyPackages.${system};
+
+      buildInputs = [
+        pkgs_old.texlive.combined.scheme-full # use as rungs has been removed
+        pkgs.gnused
+      ];
+      pname = "dnd-char-sheet";
+      version = "1.0";
+      envVars = {
+        FONTCONFIG_FILE = pkgs.makeFontsConf {fontDirectories = [./template/fonts];};
+        TEXINPUTS = "${builtins.toString ./.}//:${dnd}//:";
+      };
+    in {
+      packages.default = pkgs.stdenv.mkDerivation (pkgs.lib.recursiveUpdate {
+          name = "${pname}-${version}";
+
+          src = ./.;
+
+          inherit buildInputs;
+
+          patchPhase = ''
+            sed -i '2s|Path=template/fonts/,Extension=.ttf,||' character-sheet-settings.tex
+
+            # the following input should be safe, as nix builds things in a sandbox.
+            cat << EOF > latexmkrc
+            \$xdvipdfmx = 'xdvipdfmx -E -o %D -D "rungs -q -dALLOWPSTRANSPARENCY -dNOSAFER -dNOPAUSE -dBATCH -dEPSCrop -sPAPERSIZE=a0 -sDEVICE=pdfwrite -dCompatibilityLevel=%v -dAutoFilterGrayImages=false -dGrayImageFilter=/FlateEncode -dAutoFilterColorImages=false -dColorImageFilter=/FlateEncode -dAutoRotatePages=/None -sOutputFile=\\'%o\\' \\'%i\\' -c quit" %O %S';
+            EOF
+          '';
+
+          buildPhase = ''
+            latexmk -file-line-error -xelatex -outdir=build characters/dagmar.tex
+          '';
+
+          installPhase = ''
+            mkdir --parents $out
+            cp build/dagmar.pdf $out/
+          '';
+        }
+        envVars);
+      devShells.default = pkgs.mkShell (pkgs.lib.recursiveUpdate {
+          packages = with pkgs; [
+            nil
+            alejandra
+            statix
+            ltex-ls
+
+            texlab
+            zathura
+          ];
+          inherit buildInputs;
+        }
+        envVars);
+    });
+}
+# vim: ts=2
+
