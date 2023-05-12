cbe93bbfb23fcac116d88577f6b650c36a9f65f4
Mathew Savage <mathew.savage@infinityworks.com>
2023-05-12 15:37:14 +0100

    updates flake for character sheet

--- flake.nix
+++ flake.nix
@@ -55,8 +55,7 @@ nixpkgs_old.url = "github:NixOS/nixpkgs/release-22.11";
           '';
 
           buildPhase = ''
-            cd characters/unnamed
-            latexmk -file-line-error -xelatex -outdir=build sheet.tex
+            latexmk -file-line-error -xelatex -outdir=build unnamed/sheet.tex
           '';
 
           installPhase = ''
