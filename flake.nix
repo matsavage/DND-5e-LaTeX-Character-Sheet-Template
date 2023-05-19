d6559dae7f10adc3b51da0d4d568246cf1df02f2
Soispha <soispha@vhack.eu>
2023-05-19 06:06:11 +0000

    Update the Nix flake (#20)
    * Perf(flake): Only specify needed tex dependencies
    
    This change results in a decrease in closure size by ca. 3000MB
    (from 3.1GB to 102.3MB). This was checked with `nix path-info -Sh`.
    
    * Feat(flake): Generate package outputs for each character sheet
    
    Instead of hard coding the path to the character sheet, this change will
    generate a flake output for each character sheet.
    
    This is accomplished by generating a compile target for each file in the
    `characters` directory; all other fs-entities, i.e., directories, are ignored.
    
    Additionally the first character sheet will be set as default package.
    This is determined by sorting the names alphabetically.
    
    Example:
    Suppose that the `characters` directory contains two files (`alpha.tex`
    and `beta.tex`) and three directories (`unnamed`, `alpha` and `beta`).
    This will result in following nix flake outputs, as seen by running
    `nix flake show`:
    ```
    git+file://<path_to_flake>
    └───packages
        └───x86_64-linux
            ├───alpha: package 'alpha.tex-dnd-char-sheet-1.0'
            ├───beta: package 'beta.tex-dnd-char-sheet-1.0'
            └───default: package 'alpha.tex-dnd-char-sheet-1.0'
    ```
    
    * Build(.gitignore): Add result directory, as nix generates it
    
    * Build(flake): Update to newest dependencies
    
    The flake pins its dependencies, thus these need to be updated from time
    to time to keep them up-to-date.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-nixpkgs_old.url = "github:NixOS/nixpkgs/release-22.11";
 
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs = {};
     };
 
+    # latex specific
     dnd = {
       url = "github:rpgtex/DND-5e-LaTeX-Template";
       flake = false;
@@ -18,53 +18,151 @@ nixpkgs_old.url = "github:NixOS/nixpkgs/release-22.11";
 
   outputs = {
     nixpkgs,
-    nixpkgs_old,
     flake-utils,
     dnd,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      pkgs_old = nixpkgs_old.legacyPackages.${system};
+
+      texlive = pkgs.texlive.combine {
+        # inherits {{{
+        inherit
+          (pkgs.texlive)
+          scheme-minimal
+          # executables:
+
+          latexmk
+          xetex
+          # packages:
+
+          ## For DND 5e latex template (see: their packages.txt):
+
+          amsfonts
+          atbegshi
+          babel
+          babel-english
+          bookman
+          cfr-initials
+          cm
+          colortbl
+          contour
+          courier
+          ec
+          enumitem
+          environ
+          epstopdf-pkg
+          etex-pkg
+          etoolbox
+          fancyhdr
+          fontaxes
+          fp
+          geometry
+          gillius
+          hang
+          initials
+          keycommand
+          kpfonts
+          kvsetkeys
+          l3backend
+          l3kernel
+          l3packages
+          latex-fonts
+          lettrine
+          lm
+          ltxcmds
+          luacolor
+          microtype
+          minifp
+          ms
+          numprint
+          oberdiek
+          pgf
+          psnfss
+          ragged2e
+          tcolorbox
+          titlesec
+          tocloft
+          tools
+          trimspaces
+          was
+          xcolor
+          xkeyval
+          xstring
+          ## For this project:
+
+          fontspec
+          gensymb
+          kpfonts-otf
+          pdfcol
+          pstricks
+          tikzfill
+          ### Needed for xdv to pdf:
+
+          pst-tools
+          xetex-pstricks
+          ;
+
+        #}}}
+      };
 
       buildInputs = [
-        pkgs_old.texlive.combined.scheme-full # use as rungs has been removed
+        texlive
+        pkgs.ghostscript_headless # needed for the xdv to pdf step
         pkgs.gnused
       ];
+
       pname = "dnd-char-sheet";
       version = "1.0";
       envVars = {
         FONTCONFIG_FILE = pkgs.makeFontsConf {fontDirectories = [./template/fonts];};
         TEXINPUTS = "${builtins.toString ./.}//:${dnd}//:";
       };
+      static_packages = pkgs.lib.filterAttrs (key: value: pkgs.lib.isDerivation value) (
+        pkgs.lib.attrsets.mapAttrs' (
+          file_name: state:
+            if state == "regular"
+            then let
+              file_name_cleaned = pkgs.lib.removeSuffix ".tex" file_name;
+              value_derivation = pkgs.stdenv.mkDerivation (pkgs.lib.recursiveUpdate {
+                  name = "${file_name}-${pname}-${version}";
+
+                  src = ./.;
+
+                  inherit buildInputs;
+
+                  patchPhase = ''
+                    sed -i '9s|Path=template/fonts/,Extension=.ttf,||' dndtemplate.sty
+
+                    # -dNOSAFER is safe in this context, as nix builds things in a sandbox
+                    cat << EOF > latexmkrc
+                    \$xdvipdfmx = 'xdvipdfmx -E -o %D -D "gs -q -dALLOWPSTRANSPARENCY -dNOSAFER -dNOPAUSE -dBATCH -dEPSCrop -sPAPERSIZE=a0 -sDEVICE=pdfwrite -dCompatibilityLevel=%v -dAutoFilterGrayImages=false -dGrayImageFilter=/FlateEncode -dAutoFilterColorImages=false -dColorImageFilter=/FlateEncode -dAutoRotatePages=/None -sOutputFile=\\'%o\\' \\'%i\\' -c quit" %O %S';
+                    EOF
+                  '';
+
+                  buildPhase = ''
+                    latexmk -file-line-error -xelatex characters/${file_name}
+                  '';
+
+                  installPhase = ''
+                    mkdir --parents $out/logs
+                    cp ./*.log $out/logs
+                    cp ${file_name_cleaned}.pdf $out
+                  '';
+                }
+                envVars);
+            in
+              pkgs.lib.nameValuePair file_name_cleaned value_derivation
+            else pkgs.lib.nameValuePair "${file_name}.directory" state
+        ) (builtins.readDir ./characters)
+      );
+      default_package = static_packages."${builtins.head (builtins.attrNames static_packages)}";
     in {
-      packages.default = pkgs.stdenv.mkDerivation (pkgs.lib.recursiveUpdate {
-          name = "${pname}-${version}";
-
-          src = ./.;
-
-          inherit buildInputs;
-
-          patchPhase = ''
-            sed -i '2s|Path=template/fonts/,Extension=.ttf,||' dndtemplate.sty
-
-            # the following input should be safe, as nix builds things in a sandbox.
-            cat << EOF > latexmkrc
-            \$xdvipdfmx = 'xdvipdfmx -E -o %D -D "rungs -q -dALLOWPSTRANSPARENCY -dNOSAFER -dNOPAUSE -dBATCH -dEPSCrop -sPAPERSIZE=a0 -sDEVICE=pdfwrite -dCompatibilityLevel=%v -dAutoFilterGrayImages=false -dGrayImageFilter=/FlateEncode -dAutoFilterColorImages=false -dColorImageFilter=/FlateEncode -dAutoRotatePages=/None -sOutputFile=\\'%o\\' \\'%i\\' -c quit" %O %S';
-            EOF
-          '';
-
-          buildPhase = ''
-            latexmk -file-line-error -xelatex -outdir=build characters/unnamed/sheet.tex
-          '';
-
-          installPhase = ''
-            mkdir --parents $out/logs
-            cp build/*.log $out/logs
-            cp build/*.pdf $out
-          '';
+      packages =
+        pkgs.lib.recursiveUpdate {
+          default = default_package;
         }
-        envVars);
+        static_packages;
       devShells.default = pkgs.mkShell (pkgs.lib.recursiveUpdate {
           packages = with pkgs; [
             nil
